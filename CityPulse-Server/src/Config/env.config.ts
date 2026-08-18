bb6e22702f55c707461eb0c4fef575fb87394ae0
Mariem Mohamed <mrymmohamed38@gmail.com>
2026-08-18 22:17:37 +0300

    Fix server config and module setup

--- CityPulse-Server/src/Config/env.config.ts
+++ CityPulse-Server/src/Config/env.config.ts
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env={
-    mongoUri: process.env.MONGO_URI||"",
-    jwtAccessSecret: process.env.JWT_ACCESS_SECRET||"",
-    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET||"",
+    mongoUri: process.env.MONGO_URI??"",
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET??"",
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET??"",
+    PORT:Number(process.env.PORT)
 }
 
