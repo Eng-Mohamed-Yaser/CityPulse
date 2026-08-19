6a4d4ecc4bbd8d52d244ed384a99bb2270d35edb
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-19 18:36:13 +0300

    Initialize Express server.

---
+++ CityPulse-Server/src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app.js";
+import { env } from "./config/env.config.js";
+
+const PORT = env.port;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
