737d96d9a9108a7e8b1774efe1376f92735ea1ae
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-20 14:46:14 +0300

    Update issueGroupe modle and service

--- CityPulse-Server/src/server.ts
+++ CityPulse-Server/src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { env } from "./config/env.config.js";
 
-const PORT = env.port;
+const PORT = env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
