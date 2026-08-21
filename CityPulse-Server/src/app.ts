d5142b21cbe6cf8108e2b2843fd78af9349705b4
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-21 15:23:38 +0300

    Adding reports router to app.ts

--- CityPulse-Server/src/app.ts
+++ CityPulse-Server/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from "cors";
 import issueGroupsRouter from './routes/issueGroups.routes.js';
-import { connectDB } from './config/database.js'
+import reportsRouter from "./routes/reports.routes.js";
 import { notFound } from './middleware/notFound.middleware.js';
+import {errorHandler} from './middleware/errorHandler.middleware.js';
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.use(cors({
 }))
 
 app.use(express.json());
+app.use('/api/reports',reportsRouter)
 app.use('/api/issue-groups', issueGroupsRouter);
 app.use(notFound)
-connectDB();
+app.use(errorHandler)
 
 export default app;
