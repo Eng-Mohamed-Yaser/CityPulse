db64e8d201b05231f2d8d6d8d1b9d9d199e50a30
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-21 15:28:34 +0300

    Add function startServer to server.ts file

--- CityPulse-Server/src/server.ts
+++ CityPulse-Server/src/server.ts
@@ -1,8 +1,20 @@
 import app from "./app.js";
+import { connectDB } from "./config/database.js";
 import { env } from "./config/env.config.js";
 
 const PORT = env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  try{
+    await connectDB();
+
+    app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
