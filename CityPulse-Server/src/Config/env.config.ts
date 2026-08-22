4c60a2b710fc9d4f543d8dd45b6dd13d30db546f
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-22 14:18:11 +0300

    Update env.config.ts to include jwt expiration data for access and refresh tokens

--- CityPulse-Server/src/Config/env.config.ts
+++ CityPulse-Server/src/Config/env.config.ts
@@ -2,9 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const env={
-    mongoUri: process.env.MONGO_URI??"",
-    jwtAccessSecret: process.env.JWT_ACCESS_SECRET??"",
-    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET??"",
-    PORT:Number(process.env.PORT)
+export const env = {
+    mongoUri: process.env.MONGO_URI ?? "",
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET ?? "",
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET ?? "",
+    jwtAccessExpiration: process.env.JWT_ACCESS_EXPIRES_IN ?? "15",
+    jwtRefreshExpiration: process.env.JWT_REFRESH_EXPIRES_IN ?? "7d",
+    PORT: Number(process.env.PORT)
 }
