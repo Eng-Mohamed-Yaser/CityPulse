a582cc91f6587491daaa180709efe60bad7c2b65
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-22 15:43:38 +0300

    adding auth routes

---
+++ CityPulse-Server/src/routes/auth.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+
+import { register, login, refresh, logout, } from '../controllers/auth.controller.js';
+
+import { registerValidator, loginValidator, } from '../validators/auth.validator.js';
+
+import { validate } from '../middleware/validation.middleware.js';
+
+const router = Router();
+
+router.post(
+    '/register',
+    registerValidator,
+    validate,
+    register
+);
+
+router.post(
+    '/login',
+    loginValidator,
+    validate,
+    login
+);
+
+router.post(
+    '/refresh',
+    refresh
+);
+
+router.post(
+    '/logout',
+    logout
+);
+
+export default router;
