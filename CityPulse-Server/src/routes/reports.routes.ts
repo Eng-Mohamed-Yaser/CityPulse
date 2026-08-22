7a892c447909c9473a824ac2c8eed0188b5e87c8
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-22 16:04:39 +0300

    update reports router

--- CityPulse-Server/src/routes/reports.routes.ts
+++ CityPulse-Server/src/routes/reports.routes.ts
@@ -1,20 +1,58 @@
 import { Router } from "express";
+
 import {
     createReport,
     getAllReports,
     getReportById,
     updateReport,
-    deleteReport
+    deleteReport,
 } from "../controllers/reports.controller.js";
+
 import { validateReport } from "../validators/reports.validator.js";
-import { validate } from "../middleware/validate.middleware.js";
+import { validate } from "../middleware/validation.middleware.js";
+
+import { authenticate } from "../middleware/auth.middleware.js";
+import { authorize } from "../middleware/authorize.middleware.js";
 
 const router = Router();
 
-router.post("/", validateReport, validate, createReport);
-router.get("/", getAllReports);
-router.get("/:id", getReportById);
-router.put("/:id", validateReport, validate, updateReport);
-router.delete("/:id", deleteReport);
+router.post(
+    "/",
+    authenticate,
+    validateReport,
+    validate,
+    createReport
+);
+
+
+router.get(
+    "/",
+    authenticate,
+    getAllReports
+);
+
+
+router.get(
+    "/:id",
+    authenticate,
+    getReportById
+);
+
+
+router.put(
+    "/:id",
+    authenticate,
+    authorize("Admin", "User"),
+    validateReport,
+    validate,
+    updateReport
+);
+
+router.delete(
+    "/:id",
+    authenticate,
+    authorize("Admin"),
+    deleteReport
+);
 
 export default router;
