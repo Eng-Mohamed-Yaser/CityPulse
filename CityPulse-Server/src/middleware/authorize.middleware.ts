d9dbabedafc4168d2b0b731a24638726f0dfe5ef
Mohamed Yasser Hassan <163660396+Eng-Mohamed-Yaser@users.noreply.github.com>
2026-08-22 15:30:29 +0300

    add authurizetion middleware

---
+++ CityPulse-Server/src/middleware/authorize.middleware.ts
@@ -0,0 +1,21 @@
+import type { Request, Response, NextFunction, } from 'express';
+
+import type { UserRole, } from '../models//user.models.js';
+
+import { AppError } from '../utils/appError.js';
+
+export function authorize(...allowedRoles: UserRole[]) {
+    return (
+        req: Request, _res: Response, next: NextFunction
+    ): void => {
+        if (!req.user) {
+            throw new AppError('Authentication required', 401);
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            throw new AppError('You do not have permission to perform this action', 403);
+        }
+
+        next();
+    };
+}
